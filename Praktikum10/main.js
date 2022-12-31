d061ba89defda77b9dfa672d10d03ae0dce19c77
Naliyadivani <103926791+Naliyadivani@users.noreply.github.com>
2023-01-01 05:57:10 +0700

    Praktikum 10 Pemrograman Web

---
+++ Praktikum10/main.js
@@ -0,0 +1,85 @@
+var choices = ["paper", "rock", "scissors"];
+var i = Math.floor(Math.random() * 3);//pick 0-2 randomly
+var ComChoice = choices[i];
+var UserPoints = 0;
+var ComPoints = 0;
+const resetBtn = document.getElementById("resetBtn");
+function score(){
+	var score_div = document.getElementById("score").innerHTML = UserPoints + " - " + ComPoints;
+}
+setInterval(score, 50);
+function convert(word){
+	if(word === "paper") return '<i class="far fa-hand-paper"></i>';
+	if(word === "rock") return '<i class="far fa-hand-rock"></i>';
+	return '<i class="far fa-hand-scissors"></i>'
+}
+function game(UserChoice){
+	var box = document.getElementById("challenge");
+	box.style.display = "inline-flex";
+	var userDiv = document.getElementById("YourObject");
+	userDiv.innerHTML = convert(UserChoice);
+	var comDiv = document.getElementById("ComObject");
+	comDiv.innerHTML = convert(ComChoice);
+	if(UserChoice === "paper" && ComChoice === "rock" || UserChoice === "rock" && ComChoice === "scissors" || UserChoice === "scissors" && ComChoice === "paper"){
+		win(UserChoice);
+	}
+	else if(UserChoice === ComChoice){
+		draw(UserChoice);
+	}
+	else{
+		lose(UserChoice);
+	}
+	
+    function continuGame(){
+		i = Math.floor(Math.random() * 3);
+		ComChoice = choices[i];
+		box.style.display = "none";
+	}
+	setTimeout(continuGame, 4000);
+}
+function win(bn){
+	UserPoints++;
+    document.getElementById("who").innerHTML = "You win!";
+    var bn = document.getElementById(bn);
+    bn.classList.remove("bn");
+    bn.classList.add("green");
+    setTimeout(() => {
+    	bn.classList.add("bn");
+        bn.classList.remove("green");
+    }, 4000);
+}
+function draw(bn){
+	document.getElementById("who").innerHTML = "It's a Draw.";
+	var bn = document.getElementById(bn);
+    bn.classList.remove("bn");
+    bn.classList.add("gray");
+    setTimeout(() => {
+    	bn.classList.add("bn");
+        bn.classList.remove("gray");
+    }, 4000);
+}
+function lose(bn){
+	ComPoints++;
+	document.getElementById("who").innerHTML = "You lose...";
+	var bn = document.getElementById(bn);
+    bn.classList.remove("bn");
+    bn.classList.add("red");
+    setTimeout(() => {
+    	bn.classList.add("bn");
+        bn.classList.remove("red");
+    }, 4000);
+}
+
+//reset button
+function clearMessage(game) {
+    UserPoints = 0;
+    ComPoints= 0;
+  }
+  
+  // buttons
+  function main() {
+    resetBtn.addEventListener("click", () => clearMessage(game));
+  }
+  
+  main();
+  
